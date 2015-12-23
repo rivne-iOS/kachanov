8646303abf2b8a196a6ba0515b4d0a2c1a6b0f2a
user <user@RVTEMPMACMINI04.local>
2015-12-23 12:41:44 +0200

    calc view :)

---
+++ Calc/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Calc
+//
+//  Created by user on 12/23/15.
+//  Copyright © 2015 user. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (retain, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Calc/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Calc
+//
+//  Created by user on 12/23/15.
+//  Copyright © 2015 user. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
